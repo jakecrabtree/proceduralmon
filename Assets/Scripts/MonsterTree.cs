5e307c79836f37b871ef29a5140640c3c124b67b
Jake <jakercrabtree@utexas.edu>
2018-11-18 17:34:47 -0600

    added inheritance for different tree node types

--- Assets/Scripts/MonsterTree.cs
+++ Assets/Scripts/MonsterTree.cs
@@ -7,8 +7,20 @@ public class MonsterTree {
 	public MonsterTree breed(MonsterTree tree){
 		return this;
 	}
-	private class MonsterTreeNode {
-		public MonsterTreeNode[] children = new MonsterTreeNode[20];
-		public GameObject obj;
+}
+public abstract class MonsterTreeNode {
+	protected MonsterTreeNode[] children;
+	protected GameObject obj;
+	protected int parent;
+	public abstract Vector3 getPositionOfChild(int child);
+}
+
+public class CubeTreeNode : MonsterTreeNode { 
+	CubeTreeNode(int parent = -1){
+		children = new MonsterTreeNode[20];
+		//TODO: obj = makecube
+	}
+	public override Vector3 getPositionOfChild(int child){
+		return new Vector3();
 	}
 }
