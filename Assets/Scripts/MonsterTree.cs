9e71803ce33f0ecf2084462700eda1c7c40a35f1
Jake <jakercrabtree@utexas.edu>
2018-11-18 16:45:35 -0600

    monster tree and empty breed method

--- Assets/Scripts/MonsterTree.cs
+++ Assets/Scripts/MonsterTree.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class MonsterTree {
+	MonsterTreeNode root;
+
+	public MonsterTree breed(MonsterTree tree){
+		return this;
+	}
+}
+
 public class MonsterTreeNode {
 	MonsterTreeNode[] children = new MonsterTreeNode[20];
 	GameObject obj;
