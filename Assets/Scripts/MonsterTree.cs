eeaad3875c36be30f0d5876223a0397c18cac939
flamefire333 <flamefire333@gmail.com>
2018-11-18 17:59:19 -0600

    Some work on tree for generation

--- Assets/Scripts/MonsterTree.cs
+++ Assets/Scripts/MonsterTree.cs
@@ -11,4 +11,14 @@ public class MonsterTree {
 		public MonsterTreeNode[] children = new MonsterTreeNode[20];
 		public GameObject obj;
 	}
+	public GameObject generateMonster() {
+		GameObject o = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		Rigidbody rb = o.AddComponent<Rigidbody>();
+		GameObject o2 = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		Rigidbody rb2 = o2.AddComponent<Rigidbody> (); 
+		o2.transform.Translate (new Vector3 (1, 0, 0));
+		HingeJoint hj = o2.AddComponent<HingeJoint> ();
+		hj.connectedBody = rb;
+		return o;
+	}
 }
