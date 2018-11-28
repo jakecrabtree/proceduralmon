856c609369878b4e3da25696aa0a20955aacde42
Billy Vo <billyvo@utexas.edu>
2018-11-28 17:25:33 -0600

    Some InstructionSet changes

---
+++ Assets/Scripts/RandomizeInstructionSet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeInstructionSet{
+
+	public InstructionSet RandomizeInstructionSet(InstructionSet iSet){
+		return iSet;
+	}
+
+	public InstructionSet createDeepCopy(){
+		InstructionSet iSet = new InstructionSet();
+		//for(int i = 0; i < )
+	}
+}
