7c7593baf633b54d88e401795a5b4dacb971d5f7
Billy Vo <billyvo@utexas.edu>
2018-12-05 21:15:09 -0600

    Save function (incomplete)

--- Assets/TestScripts/FitnessFunction.cs
+++ Assets/TestScripts/FitnessFunction.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Runtime.Serialization;
+using System.IO;
+
 public class FitnessFunction : MonoBehaviour {
     public Transform creature;
     public float timeScale = 1;
@@ -26,5 +29,19 @@ public class FitnessFunction : MonoBehaviour {
     void FixedUpdate ()
     {
         distanceTravelled = Vector3.Distance(new Vector3(0, 0, 0), creature.position);
+    }
+
+    void Save()
+    {
+        //BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/creatureData.dat", FileMode.Open);
+
+        CreatureData data = new CreatureData();
+    }
+
+    class CreatureData
+    {
+        public float distanceTravelled;
+        public MonsterTree monsterTree;
     }
 }
