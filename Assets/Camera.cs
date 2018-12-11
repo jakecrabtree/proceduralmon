f796a7d53ff81a48b30d3fcd48390020e93ddd66
flamefire333 <flamefire333@gmail.com>
2018-12-10 23:14:56 -0600

    Now with camera controls

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -11,6 +11,35 @@ public class Camera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (new Vector3(0,0,1));
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (new Vector3 (0,0,-1));
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (new Vector3 (-1, 0, 0));
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (new Vector3 (1, 0, 0));
+		}
+		if (Input.GetKey (KeyCode.C)) {
+			transform.Translate (new Vector3 (0, -1, 0));
+		}
+		if (Input.GetKey (KeyCode.X)) {
+			transform.Translate (new Vector3 (0, 1, 0));
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (new Vector3(0, .4f, 0));
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (new Vector3 (0, -.4f, 0));
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Rotate (new Vector3 (-.4f, 0, 0));
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Rotate (new Vector3 (.4f, 0, 0));
+		}
 	}
 }
